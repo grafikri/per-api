8d45ec5f5c4eaf0b228e11d9be5a79581a533735
Serhan Coşkun <grafikri@gmail.com>
2021-04-14 23:33:40 +0300

    feat: build database wrapper

---
+++ database/analytics.js
@@ -0,0 +1,40 @@
+
+const db = require("./firebase");
+const analytics = db.ref('analytics')
+
+/**
+ * 
+ * @param {*} data 
+ * @returns {string} saved data id
+ */
+const save = (data) => {
+  let ref = analytics.push()
+  ref.set(data)
+  return ref.key
+}
+
+
+/**
+ * 
+ * @param {number} startDate timestamp 
+ * @param {number} endDate timestamp
+ * 
+ * @returns {Promise<any>}
+ */
+const fetch = (startDate, endDate) => {
+  return new Promise(resolve => {
+    let ref = analytics.orderByChild('date')
+
+    if (startDate) ref = ref.startAt(startDate)
+    if (endDate) ref = ref.endAt(endDate)
+
+    ref.once("value", function (data) {
+      resolve(data.val() || {})
+    });
+  })
+}
+
+module.exports = {
+  save,
+  fetch
+}
