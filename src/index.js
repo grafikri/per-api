6feacc74fd01689890981b14ed1b15f086eb3f89
Serhan Coşkun <grafikri@gmail.com>
2021-04-17 10:49:04 +0300

    feat: activate cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const db = require('./../database')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(express.json());
 app.use(express.text());
 
